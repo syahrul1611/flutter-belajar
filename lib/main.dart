1ffc5fcc6b1b90e43adbc21343ba7a2acfa088d1
syahrulR11 <114170207+syahrulR11@users.noreply.github.com>
2023-02-15 13:37:34 +0700

    eps 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,26 @@
-// ignore_for_file: unnecessary_const
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TheApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class TheApp extends StatelessWidget {
+  const TheApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
+      title: 'App',
       theme: ThemeData(
-          primarySwatch: Colors.teal,
+          primarySwatch: Colors.lightBlue,
           visualDensity: VisualDensity.adaptivePlatformDensity),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Judul'),
-        ),
-        body: const Center(
-          child: Text('Ini sub judul'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.play_arrow),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: const [
-              DrawerHeader(
-                decoration: BoxDecoration(color: Colors.lightBlue),
-                child: Text('Drawer Header'),
-              ),
-              ListTile(title: Text('Ini Tile 1'))
-            ],
+          appBar: AppBar(
+            title: Text('Apps'),
           ),
-        ),
-      ),
+          body: Center(
+            child: Text('Hello'),
+          )),
     );
   }
 }
